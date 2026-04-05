d87a3067e9b2b545208f20354199d688dbe149f8
hakisolos <maxwellexcel174@gmail.com>
2026-04-05 21:48:46 +0100

    workflow automation

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/hakisolos/waitlist/internals"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+	}))
 	r.GET("/", internals.Home)
 	r.POST("/waitlist/join", internals.AddToWaitlist)
 	r.GET("/waitlist/get", internals.GetWaitlistEmail)
